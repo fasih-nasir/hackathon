95c67d63e698e81424bddce05fba1e9654db9c2a
fasihnasir <154458171+fasih-nasir@users.noreply.github.com>
2024-08-17 16:55:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Top header date
-
+import {auth, getDocs, updateDoc,doc, deleteDoc ,signOut, collection, addDoc, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, provider, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "./files/config.mjs"
 var date=new Date();
 document.getElementById("dat").innerHTML=`${date.toDateString()}`
 
@@ -13,7 +13,7 @@ setTimeout(() => {
 );
 window.addEventListener("scroll",()=>{
 var nav=document.querySelector(".navbc");
-console.log(window.scrollY);
+// console.log(window.scrollY);
 
 if(window.scrollY > 200 ){
     nav.classList.add("topnav")
@@ -22,3 +22,49 @@ else{
     nav.classList.remove("topnav")
 }
 })
+
+
+// var  create1=document.getElementById("cret");
+
+// create1.addEventListener("click", (r)=>{
+
+// var email0=document.getElementById("email0").value;
+// var password0=document.getElementById("pass0").value;
+// var name0=document.getElementById("name0").value;
+// var dob0=document.getElementById("dob").value;
+// var gender0=document.getElementById("gender").value;
+// var age0=document.getElementById("age").value;
+// var country0=document.getElementById("country").value;
+// var profession0=document.getElementById("profession").value;
+// try {
+//     const date = new Date();
+//       const docRef =  addDoc(collection(db, "hack"), {
+//         name: name0,
+//         email:email0,
+//       dob:dob0,
+//       gender:gender0,
+//       age:age0,
+//       profession:profession0,
+//       country:country0,
+//       day:date.toString().split(" ").slice(0,5)
+  
+//       });
+//       console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//       console.error("Error adding document: ", e);
+//     }
+// createUserWithEmailAndPassword(auth, email0, password0)
+//   .then((userCredential) => {
+//     // Signed up 
+//     const user = userCredential.user;
+//     alert("account is created")
+//     // ...
+// })
+// .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+// });
+
+// })
+
